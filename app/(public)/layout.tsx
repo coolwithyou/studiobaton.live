c09a3c4b7505d1741f3aac4ebac4b085f872142f
coolwithyou <sw@ba-ton.kr>
2026-01-19 10:00:58 +0900

    feat: Add dark and light logo assets and comment out logo display in public layout.

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -15,16 +15,17 @@ export default function PublicLayout({
       {/* 왼쪽 고정 로고 - container 그리드에 맞춤, 모바일에서 숨김 */}
       <div className="fixed top-24 left-0 right-0 z-40 pointer-events-none hidden lg:block">
         <div className="container mx-auto px-4">
-          <Link href="/" className="pointer-events-auto inline-block">
+          {/* <Link href="/" className="pointer-events-auto inline-block">
             <Image
-              src="/log_baton_logo.png"
+              src="/log_baton_logo_dark.png"
               alt="studiobaton log"
               width={140}
               height={160}
+              className="opacity-10"
               // className="rounded-xl"
               priority
             />
-          </Link>
+          </Link> */}
         </div>
       </div>
 
